4427b404abffa99e91caef27e4da248079ea9505
JayStark0316 <jaystark0316@gmail.com>
2026-02-06 08:47:19 -0500

    New capabilities added to the application - commons

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
 
 from commons.database import db_manager
+from commons.exceptions.base_api_exception_handler import BaseAPIException
+from commons.exceptions.default_error_api_response import DefaultApiErrorResponse
 
+
+# ------------------------------
+# Logging Configuration
+# ------------------------------
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -16,6 +23,9 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+# ------------------------------
+# Life Span Handler
+# ------------------------------
 @asynccontextmanager
 async def lifespan(app):
     try:
@@ -28,8 +38,21 @@ async def lifespan(app):
     finally:
         logger.info("Closing database connection...")
 
-app = FastAPI(lifespan=lifespan)
 
-@app.get("/")
-async def hello_world():
-    return {"message": "Hello World"}
+# ------------------------------
+# App Initialization
+# ------------------------------
+app = FastAPI(lifespan=lifespan, root_path="/api/v1")
+
+
+# ------------------------------
+# Application Exception Handler
+# ------------------------------
+@app.exception_handler(BaseAPIException)
+async def app_execution_handler(request, exc):
+    api_error = DefaultApiErrorResponse(
+        status_code=exc.status_code,
+        message=exc.message
+    )
+    return JSONResponse(status_code=exc.status_code, content=api_error.__dict__)
+
