64d0bc072f959fea5d029a713d77e9ed8c62221d
JayStark0316 <jaystark0316@gmail.com>
2026-02-07 13:21:15 -0500

    Adding tests to the application

--- users/user_repository.py
+++ users/user_repository.py
@@ -1,14 +1,22 @@
 from pymongo.synchronous.database import Database
-
 from users.db_user_model import UserDbModel
+from uuid import uuid4
 
 COLLECTION_NAME = "users"
 
-def get_user_by_id(id: str, db: Database):
-    pass
+def get_user_by_id(user_id: str, db: Database) -> UserDbModel | None:
+    _document = db.get_collection(COLLECTION_NAME).find_one({"_id": user_id})
+    return UserDbModel(**_document) if _document else None
+
 
 def get_user_by_hashed_email(email: str, db: Database):
-    pass
+    _document = db.get_collection(COLLECTION_NAME).find_one({"hashed_email": email})
+    return UserDbModel(**_document) if _document else None
 
-def create_user(user_db: UserDbModel):
-    pass
+
+def create_user(user_db: UserDbModel, db: Database):
+    model_db = user_db.model_dump(by_alias=True)
+    generated_id = uuid4()
+    model_db['_id'] = generated_id
+    inserted_id = db.get_collection(COLLECTION_NAME).insert_one(model_db).inserted_id
+    return inserted_id
